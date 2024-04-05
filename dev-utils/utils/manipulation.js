a03cb419186d05dcbc7cd459d21c4afd8f5fc694
Yaron Galperin <yaronglp@gmail.com>
2024-04-05 14:01:55 +0300

    chore: prettier test

--- dev-utils/utils/manipulation.js
+++ dev-utils/utils/manipulation.js
@@ -3,5 +3,8 @@ export function toPascalCase(str) {
     return
   }
 
-  return str.split(" ").map((word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()).join("")
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase())
+    .join("")
 }
