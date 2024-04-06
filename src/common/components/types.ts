a1edc24b340190442c5c3da807412596f184ecc2
Yaron Galperin <yaronglp@gmail.com>
2024-04-06 12:47:40 +0300

    chore: add common types

---
+++ src/common/components/types.ts
@@ -0,0 +1,11 @@
+export interface PPCustomAttributes {
+  "data-selector"?: string
+  "data-test-id"?: string
+}
+
+export interface PPAccessibility {
+  "aria-label"?: string
+  "aria-labelledby"?: string
+  "aria-describedby"?: string
+  "aria-expanded"?: string
+}
